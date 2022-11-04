a7798e663ae29cf41620bf71f0af04aa170929aa
lqpinoo <bonnotte.tim@gmail.com>
2022-11-04 15:19:55 +0100

    video webm

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,41 +8,39 @@ export default function Home() {
     <>
       <section className="showcase">
         <header>
-          <h2 className="logo">:)</h2>
-          <div className="toggle"></div>
+          <h2 className="logo">
+            <img src="./logo.png" alt="logo" width="120px" height="120px" />
+          </h2>
+          {/* <div className="toggle"></div> */}
         </header>
-        <video src="./meutichair-hq.mp4" muted loop autoPlay></video>
+        <video src="./meuti-qhd_encoded.webm" muted loop autoPlay></video>
         {/* <div className="overlay"></div> */}
         <div className="text">
           <h2>Timothée</h2>
           <h3>Front-End Developer</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <a href="#">Explore</a>
+          <h4>React & React Native</h4>
+          <div className="intro">
+            <p>
+              Touche à tout et féru de design avec une grosse passion pour la
+              modélisation 3D, j'aime voir les features prendre vie et faire
+              naître de nouvelles choses grâce au code. <br /> <br /> Mon
+              parcours est orienté SASS web & mobile, mais je suis toujours
+              curieux de voir et d'apprendre sur des sujets différents.
+              <br /> <br />
+              J'ai un coup de ❤️ pour les boites early stage car il y a tout à
+              construire avec l'envie de bien faire.
+            </p>
+          </div>
         </div>
         <ul className="social">
           <li>
-            <a href="#">
-              <img src="https://i.ibb.co/x7P24fL/facebook.png" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="https://i.ibb.co/Wnxq2Nq/twitter.png" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
+            <a href="https://www.instagram.com/meutidoes3d/" target="_blank">
               <img src="https://i.ibb.co/ySwtH4B/instagram.png" />
             </a>
           </li>
         </ul>
       </section>
-      <div className="menu">
+      {/* <div className="menu">
         <ul>
           <li>
             <a href="#">Home</a>
@@ -60,7 +58,7 @@ export default function Home() {
             <a href="#">Contact</a>
           </li>
         </ul>
-      </div>
+      </div> */}
     </>
   )
 }
