dd9525069018c7e56c615bd27990381bcdd37baf
meuti <7700613+meutiti@users.noreply.github.com>
2022-12-28 12:26:29 +0100

    feat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { useState, Fragment, useEffect } from 'react'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Switch from 'react-switch'
 import {
